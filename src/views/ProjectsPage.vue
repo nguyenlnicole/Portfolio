7645dbb13eebc55e016b26de0af2a817caa09c54
Nicole Nguyen <Nguyenlnicole@gmail.com>
2023-05-30 15:41:58 -0500

    updates

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -4,28 +4,29 @@
 
     <div class="ChatRHN">
       <a href="https://github.com/raulharo/ChatRHN" target="_blank">ChatRNH</a><br>
-      Paired-Project with one other person. It is the sassy version of ChatGpt. It will NOT answer your questions, <br>
-      but responds in a sassy manner. We utilized Java, postgreSQL, SQL, API, tokens, HTML, CSS, and Jaascript.<br>
+      Paired-Project with one other person. It is the sassy version of ChatGpt. User can ask the <br>
+      program questions, but it will respond in a sassy manner without answering the question. <br>
+      We utilized Java, postgreSQL, SQL, API, tokens, HTML, CSS, and Jaascript.<br>
 
       </div>
 
     <div class= "portfolio">
       <a href="https://github.com/nguyenlnicole/PortfolioWebPage" target="_blank">Portfolio</a><br>
   
-      This portfolio that you are currently looking at. I built it from scratch. It is meant to display infromation on myself <br>
-       my projects, and my contact information. I utilized Javascript, Vue.js, html, and CSS. 
+      This portfolio that you are currently looking at. I built it from scratch. It is meant to display  <br>
+     infromation on myself my projects, and my contact information. I utilized Javascript, Vue.js, html, and CSS. 
       </div>
 
     <div class= "flashCards">
       <a href="https://github.com/nguyenlnicole/FlashCards" target="_blank">Flash Cards</a><br>
-      My first solo project that I bult from scratch. It is quiz like application in Java, using object of  <br>
+      My first solo project that I built from scratch. It is quiz-like application in Java, using the object of  <br>
       scanner, conditional statement, and method for printing out messages randomly. This is meant to help <br>
-      students study for Java voacabulary<br>
+      students study Java voacabulary and codes<br>
       </div>
 
       <div class= "eightball">
         <a href="https://github.com/nguyenlnicole/magic8Ball/tree/main/magic8ball" target="_blank">8 Ball</a><br>
-        First project done that I was given a starter code. You can ask a yes or no question, and itll reply It  <br>
+        First project done that I was given a starter code. User can ask it a yes or no question, and it will reply It  <br>
         is small programming application in Java, that allows practice using boolean expression, pseudo code that  <br>
         generates random numbers, and allows the user to print information out to the user.<br>
      
