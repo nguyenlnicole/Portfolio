4aa9a9269490b11bc8e0ab6a83f21be1f254136b
Nicole <nguyenlnicole@gmail.com>
2023-07-03 16:11:41 -0500

    punctionation

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -24,7 +24,7 @@
       pet listings and shelter information. Additionally, the website includes robust admin features,<br>
        allowing administrators to effortlessly add new pets, approve volunteers, update pet statuses,<br> 
        all made possible through the implementation of Java, HTML, CSS, JavaScript, JDBC, SQL, <br>
-       PostgreSQL, Vue.js, Spring Boot, and API integration, . <br>
+       PostgreSQL, Vue.js, Spring Boot, and API integration. <br>
 
     </div>
 
